479bee55072b734b0e4380c291b4a24692714eec
Andrey Fedorovich <a.d.fedorovich@gmail.com>
2016-09-09 18:06:21 +0300

    add modal invitation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160908214531) do
+ActiveRecord::Schema.define(version: 20160909134404) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -62,6 +62,14 @@ ActiveRecord::Schema.define(version: 20160908214531) do
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
     t.index ["imageable_type", "imageable_id"], name: "index_images_on_imageable_type_and_imageable_id", using: :btree
+  end
+
+  create_table "invitations", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.text     "message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "post_sections", force: :cascade do |t|
