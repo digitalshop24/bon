5293480d22291e873479531a192e27280d4bab32
K. Kanyuka <kkan.web@gmail.com>
2016-11-04 19:17:28 +0300

    Hide horizontal menu on mobiles. Burger on mobiles

--- app/assets/javascripts/new.js
+++ app/assets/javascripts/new.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  $('.horizontalMenu .burger').on('click tap', function(e){
+  $('.burger[data-menu="sidebar"]').on('click tap', function(e){
     e.preventDefault();
     $('.sidebar.menu').sidebar('setting',{ transition:'overlay' }).sidebar('toggle');
   });
