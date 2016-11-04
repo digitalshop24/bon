7366f2cb9985116598e22e8d1c61d0c75311a4ef
K. Kanyuka <kkan.web@gmail.com>
2016-11-05 00:08:53 +0300

    Image thumbnail slider

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,8 +1,11 @@
 class Image < ActiveRecord::Base
   belongs_to :imageable, polymorphic: true
 
-  has_attached_file :image, styles: { medium: "700x465#", small: "400x266#", thumb: "160x106#" },
-                    default_url: ":s3_missing_url.jpg"
+  has_attached_file :image, default_url: ":s3_missing_url.jpg",
+    styles: lambda { |a| { 
+      medium: "700x465#", small: "400x266#", thumb: "160x106#", 
+      slider: "#{a.instance.width}x#{a.instance.width/4}#" }
+    }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
   rails_admin do
