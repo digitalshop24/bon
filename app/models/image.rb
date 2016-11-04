28bfeb6f9af535ff3e5d26542b6ef17ce76b1e45
K. Kanyuka <kkan.web@gmail.com>
2016-11-05 01:51:01 +0300

    Fix slider sizes

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,10 +1,11 @@
 class Image < ActiveRecord::Base
   belongs_to :imageable, polymorphic: true
+  attr_reader :width
 
   has_attached_file :image, default_url: ":s3_missing_url.jpg",
-    styles: lambda { |a| { 
-      medium: "700x465#", small: "400x266#", thumb: "160x106#", 
-      slider: "#{a.instance.width}x#{a.instance.width/4}#" }
+  styles: {
+      medium: "700x465#", small: "400x266#", thumb: "160x106#",
+      slider:  Proc.new { |a| a.slider_size }
     }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
@@ -12,5 +13,10 @@ class Image < ActiveRecord::Base
     edit do
       field :image
     end
+  end
+
+  def slider_size
+    geometry = Paperclip::Geometry.from_file(Paperclip.io_adapters.for(image))
+    "#{geometry.width.to_i}x#{geometry.width.to_i/4}#"
   end
 end
