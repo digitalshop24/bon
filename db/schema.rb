5f9161ac8a178d5c01424d2cde703b238b08dad6
K. Kanyuka <kkan.web@gmail.com>
2017-02-07 19:16:13 +0300

    Added gallery to post

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170203012239) do
+ActiveRecord::Schema.define(version: 20170207151330) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,14 @@ ActiveRecord::Schema.define(version: 20170203012239) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["post_id"], name: "index_comments_on_post_id", using: :btree
+  end
+
+  create_table "galleries", force: :cascade do |t|
+    t.string   "galleryable_type"
+    t.integer  "galleryable_id"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+    t.index ["galleryable_type", "galleryable_id"], name: "index_galleries_on_galleryable_type_and_galleryable_id", using: :btree
   end
 
   create_table "images", force: :cascade do |t|
